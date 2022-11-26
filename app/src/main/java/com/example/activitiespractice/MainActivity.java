b1e4c7d2599d58c7f244b0e9c417e1d046b40204
mzaman21 <zwordpressservices@gmail.com>
2022-11-26 08:01:15 +0500

    on click listner added

--- app/src/main/java/com/example/activitiespractice/MainActivity.java
+++ app/src/main/java/com/example/activitiespractice/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.activitiespractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button nextbutn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nextbutn = findViewById(R.id.nextbtn);
+
+        nextbutn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/activitiespractice/MynewActivity.java
@@ -0,0 +1,14 @@
+package com.example.activitiespractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MynewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mynew);
+    }
+}
