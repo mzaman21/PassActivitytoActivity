79723c1871d6ca94630010870db0dc819b1ab4fc
mzaman21 <zwordpressservices@gmail.com>
2022-11-26 08:23:57 +0500

    settings for getting passed data from another activity

--- app/src/main/java/com/example/activitiespractice/MynewActivity.java
+++ app/src/main/java/com/example/activitiespractice/MynewActivity.java
@@ -3,12 +3,22 @@ package com.example.activitiespractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MynewActivity extends AppCompatActivity {
 
+    String StudentName,StudentRollNo;
+    TextView Name,RollNo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mynew);
+
+        Name= findViewById(R.id.Sname);
+        RollNo=findViewById(R.id.Srollno);
+
+
+
+
     }
 }
